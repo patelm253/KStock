0969915c62c1ec5c62d68a31495674889bbc94f3
Mayur <patelm253@gmail.com>
2019-07-13 17:52:58 +0530

    First commit

---
+++ src/kstock/KStock.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kstock;
+
+/**
+ *
+ * @author Mayur
+ */
+public class KStock {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                MainStock frame= new MainStock();
+                
+                //frame.setExtendedState(MainStock.MAXIMIZED_BOTH);
+                frame.setVisible(true);
+                //new Login().setVisible(true);
+            }
+        });
+        
+    }
+    
+}
